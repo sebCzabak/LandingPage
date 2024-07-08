6e5ae2a0156505be9566b496ff1f2d0910ad0cc9
sebCzabak <sczabak@gmail.com>
2024-07-08 10:58:13 +0200

    Dodaj pliki projektów.

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,25 @@
+﻿using LandingPage.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LandingPage.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(ContactFormModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                //TODO
+                TempData["Message"] = "Thank you for contacting us!";
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+    }
+}
---
+++ Models/ContactFormModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace LandingPage.Models
+{
+    public class ContactFormModel
+    {
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string Message {  get; set; }
+    }
+}
